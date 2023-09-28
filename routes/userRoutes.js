f937afe19a782ad6823571b817f7ecae9cd4064d
Adetoki-timilehin <75859586+Adetoki-timilehin@users.noreply.github.com>
2023-09-28 23:30:11 +0100

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "get users" });
+});
+
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "create user" });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `upadte user with id: ${req.params.id}`});
+});
+
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `get user with id: ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `delete user with id: ${req.params.id}` });
+});
+
+module.exports = router;
